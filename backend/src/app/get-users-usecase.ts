bef34e1a37e1f4ae4f708e8b6cb62c2ff14c5c0c
kai815 <h.kaizuka815@gmail.com>
2021-12-19 12:20:17 +0900

    ユーザー取得のusecase

---
+++ backend/src/app/get-users-usecase.ts
@@ -0,0 +1,16 @@
+import { IUsersQS } from './query-service-interface/users-qs'
+
+export class GetUsersUseCase {
+  private readonly userQS: IUsersQS
+  public constructor(userQS: IUsersQS) {
+    this.userQS = userQS
+  }
+  public async do() {
+    try {
+      return await this.userQS.getAll()
+    } catch (error) {
+      // memo: エラー処理
+      throw error
+    }
+  }
+}
