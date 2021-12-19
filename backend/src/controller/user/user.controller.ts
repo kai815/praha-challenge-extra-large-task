df85053387ff96f111a923e018602588d6541170
kai815 <h.kaizuka815@gmail.com>
2021-12-19 11:21:00 +0900

    userコントローラーの実装

---
+++ backend/src/controller/user/user.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Post } from '@nestjs/common'
+// import { ApiResponse } from '@nestjs/swagger'
+import { PostUserRequest } from './request/post-user-request'
+import { PrismaClient } from '@prisma/client'
+import { UserRepository } from 'src/infra/db/repository/user-repository'
+import { PostUserUseCase } from 'src/app/user-usecase'
+
+@Controller({
+  path: '/user',
+})
+export class UserController {
+  @Post()
+  async postUser(@Body() postUserDto: PostUserRequest): Promise<void> {
+    const prisma = new PrismaClient()
+    const repo = new UserRepository(prisma)
+    const usecase = new PostUserUseCase(repo)
+    await usecase.do({
+      name: postUserDto.name,
+      email: postUserDto.email,
+    })
+  }
+}
