3b87f79ec0bbed33523a75f6cfb2e81c259f3391
kai815 <h.kaizuka815@gmail.com>
2022-01-23 19:42:49 +0900

    usertaskのupdateのusecaseの作成

---
+++ backend/src/app/usecase/user-task/update-user-task-usecase.ts
@@ -0,0 +1,23 @@
+import { IUserTaskRepository } from 'src/app/repository-interface/user-task-repository-interface'
+import { IUserTaskFactory } from 'src/app/factory-interface/user-task-factory-interface'
+import { Status } from 'src/domain/entity/sintyoku-status'
+
+export class UpdateTaskUseCase {
+  private readonly userTaskRepo: IUserTaskRepository
+  private readonly userTaskFac: IUserTaskFactory
+  public constructor(
+    userTaskRepo: IUserTaskRepository,
+    userTaskFac: IUserTaskFactory,
+  ) {
+    this.userTaskRepo = userTaskRepo
+    this.userTaskFac = userTaskFac
+  }
+  public async do(params: { id: string; status: Status }) {
+    const userTaskEntity = await this.userTaskFac.create({
+      kind: 'Update',
+      id: params.id,
+      status: params.status,
+    })
+    await this.userTaskRepo.save(userTaskEntity)
+  }
+}
