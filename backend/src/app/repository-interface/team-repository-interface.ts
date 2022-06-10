ab17c89fa0461ad42befb8f49e3355e9b436d82b
kai815 <h.kaizuka815@gmail.com>
2022-06-11 08:53:45 +0900

    teamの名前だけ取れればいいのでメソッドを修正のinterface

--- backend/src/app/repository-interface/team-repository-interface.ts
+++ backend/src/app/repository-interface/team-repository-interface.ts
@@ -2,5 +2,5 @@ import { Team } from 'src/domain/entity/Team'
 
 export interface ITeamRepository {
   save(team: Team): Promise<Team>
-  getByNameLast(): Promise<string | null>
+  getTeamNameByNameLast(): Promise<string | null>
 }
