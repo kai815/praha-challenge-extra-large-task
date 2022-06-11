46fd4d6cd5ca78e37a96c3ce5fcfa796adcb9aab
kai815 <h.kaizuka815@gmail.com>
2022-06-11 16:22:22 +0900

    一番チームメンバーが少ないチームを返すメソッドをサービスに追加

--- backend/src/domain/service/team.service.ts
+++ backend/src/domain/service/team.service.ts
@@ -1,4 +1,5 @@
 import { ITeamRepository } from 'src/app/repository-interface/team-repository-interface'
+import { Team } from 'src/domain/entity/team'
 
 export class TeamService {
   private teamRepo: ITeamRepository
@@ -12,5 +13,16 @@ export class TeamService {
       ? (Number(lastTeamName) + 1).toString()
       : '1'
     return newTeamName
+  }
+  //一番メンバー数が少ないチームを返す(本当はrepoでやりたい)
+  public async getMinimumMemberTeam(): Promise<Team | null> {
+    const allTeams = await this.teamRepo.getAllTeam()
+    const minimumMemberTeam = allTeams.reduce((previousValue, currentValue) =>
+      //同じの場合はpreviousを大きいとする
+      previousValue.getTeamMemberCount > currentValue.getTeamMemberCount
+        ? previousValue
+        : currentValue,
+    )
+    return minimumMemberTeam
   }
 }
