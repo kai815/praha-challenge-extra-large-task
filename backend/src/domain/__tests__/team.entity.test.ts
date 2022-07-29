89f9c5b2a8f704228b04db74439b1c8e94aa2ff1
kai815 <h.kaizuka815@gmail.com>
2022-07-29 11:07:25 +0900

    ✅ teamのaddPairMembersの単体テストの追加

--- backend/src/domain/__tests__/team.entity.test.ts
+++ backend/src/domain/__tests__/team.entity.test.ts
@@ -20,6 +20,7 @@ describe('task.entity.test', () => {
     id: createRandomIdString(),
     name: '1',
   }
+  //pair1のメンバー
   const normalMember1Pair1 = {
     id: createRandomIdString(),
     pairId: normalPair1.id,
@@ -30,6 +31,12 @@ describe('task.entity.test', () => {
     pairId: normalPair1.id,
     userId: userIdMember2Pair1,
   }
+  const normalMember3Pair1 = {
+    id: createRandomIdString(),
+    pairId: normalPair1.id,
+    userId: userIdMember2Pair1,
+  }
+  //pair2のメンバー
   const normalMember1Pair2 = {
     id: createRandomIdString(),
     pairId: normalPair2.id,
@@ -146,6 +153,43 @@ describe('task.entity.test', () => {
       }).toThrow('3名未満のteamは存在できません。')
     })
   })
+  describe('[team]addPairMembers', () => {
+    it('[正常系]少ないペアにteamのメンバーが追加されている', () => {
+      const member1Pair1 = new Member(normalMember1Pair1)
+      const member2Pair1 = new Member(normalMember2Pair1)
+      const member3Pair1 = new Member(normalMember3Pair1)
+      const member1Pair2 = new Member(normalMember1Pair2)
+      const member2Pair2 = new Member(normalMember2Pair2)
+      const pair1 = new Pair({
+        ...normalPair1,
+        members: [member1Pair1, member2Pair1, member3Pair1],
+      })
+      const pair2 = new Pair({
+        ...normalPair2,
+        members: [member1Pair2, member2Pair2],
+      })
+      const team = new Team({
+        ...normalTeam,
+        pairs: [pair1, pair2],
+      })
+      const addingUserId = createRandomIdString()
+      const addingMemberId = createRandomIdString()
+      const addedMember = new Member({
+        id: addingMemberId,
+        userId: addingUserId,
+        pairId: normalPair2.id,
+      })
+      // メンバー追加された後のペア2
+      const addedPair2 = new Pair({
+        ...normalPair2,
+        members: [member1Pair2, member2Pair2, addedMember],
+      })
+      team.addPairMembers({ userId: addingUserId, memberId: addingMemberId })
+      expect(team.getAllProperties().pairs).toEqual([pair1, addedPair2])
+    })
+  })
+
+  //ここからpairのテスト
   describe('[pair]valdateName', () => {
     it('[異常系]pairの名前に半角英字以外が含まれる場合エラーを吐く', () => {
       const abnormalPair1 = {
