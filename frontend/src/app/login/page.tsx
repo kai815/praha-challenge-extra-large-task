36afcdaa2f91134613c525f2284e77c2416861c7
kai815 <h.kaizuka815@gmail.com>
2023-06-25 16:27:38 +0900

    💄 ログインチェックとログアウトの実装

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,18 +1,47 @@
  'use client'
 
-import { login } from "@/lib/login"
+ import { getAuth } from "firebase/auth";
+import { login,logout } from "@/lib/login"
 
 export default function Login () {
   const handleClickLogin = () => {
     login()
   }
+  const handleClickLogout = () => {
+    logout()
+  }
+  const handleClickLoginCheck = () => {
+    const auth = getAuth()
+    if(auth.currentUser){
+      alert(`${auth.currentUser?.displayName}でログインしてます。`)
+    }else{
+      alert('ログインしてないです。')
+    }
+    
+  }
 
   return (
     <div className="m-auto w-64">
       <h2>Login</h2>
-      <button onClick={handleClickLogin}>
-        Googleでログイン
-      </button>
+      <div>
+        <button onClick={handleClickLogin}>
+          Googleでログイン
+        </button>
+      </div>
+      <div>
+        <button onClick={handleClickLogout}>
+          ログアウト
+        </button>
+      </div>
+      <div>
+        <button onClick={handleClickLoginCheck}>
+          ログインチェック
+        </button>
+      </div>
     </div>
   )
 }
+function useSate(arg0: boolean): [any, any] {
+  throw new Error("Function not implemented.")
+}
+
